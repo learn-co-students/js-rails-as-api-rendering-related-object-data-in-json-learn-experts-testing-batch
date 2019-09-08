715cf782d36b206609a69f242a6c042075b0b0d5
github username <github email address>
2019-09-08 09:03:32 -0400

    Done.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -6,7 +6,7 @@ class SightingsController < ApplicationController
     render json: { id: sighting.id, bird: sighting.bird, location: sighting.location }
     #render json: sighting, include: [:bird, :location]
   end
-
+##
   # def show
   #   sighting = Sighting.find_by(id: params[:id])
   #   render json: sighting.to_json(:include => {
